077f93f4bc8fa5b2dfc0e0135246f274bd4efb1f
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-08-21 20:44:55 -0400

    in DashboardClient.tsx added create a store button, added form to create a store, added createStore function for post request, added visit your store button, added styling in DashboardClient.module.css, used getStoreById in page.tsx

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -1,11 +1,119 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { SubmitHandler, FieldValues, useForm } from "react-hook-form";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useState } from "react";
+import Input from "../components/inputs/Input";
+import { useRouter } from "next/navigation";
+import { Store } from "@prisma/client";
+import classes from "./DashboardClient.module.css";
 
-const DashboardClient = () => {
+interface DashboardClientProps {
+  store: Store | null;
+}
+
+const DashboardClient: React.FC<DashboardClientProps> = ({ store }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: { name: "", description: "", subdomain: "" },
+  });
+
+  const createStore: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("api/store", data)
+      .then(() => {
+        toast.success("Store Created!");
+        setIsOpen(false);
+        router.refresh();
+      })
+      .catch((error) => {
+        toast.error(error.response.data.error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div>
-      <button onClick={() => signOut()}>Sign Out</button>
+      <div className={classes.navbar}>
+        <h2>Dashboard</h2>
+        <div className={classes.navActions}>
+          {store && (
+            <button
+              onClick={() => router.push(`/store/${store?.subdomain}`)}
+              className={classes.visitStoreButton}
+            >
+              Visit your Store
+            </button>
+          )}
+          {!store && (
+            <button
+              onClick={() => setIsOpen(true)}
+              className={classes.createStoreFormButton}
+            >
+              Create a Store
+            </button>
+          )}
+          <button onClick={() => signOut()} className={classes.logoutButton}>
+            Sign Out
+          </button>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div>
+          <form onSubmit={handleSubmit(createStore)} className={classes.form}>
+            <h3>Create your store!</h3>
+            <Input
+              id="name"
+              label="Name"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+            <Input
+              id="description"
+              label="Description"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+            <Input
+              id="subdomain"
+              label="Subdomain (no spaces)"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+            <div className={classes.formActions}>
+              <button
+                onClick={() => setIsOpen(false)}
+                className={classes.cancelFormButton}
+              >
+                Cancel
+              </button>
+              <button type="submit" className={classes.createStoreButton}>
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import getStoreByUserId from "../actions/getStoreByUserId";
 import DashboardClient from "./DashboardClient";
 
-const Dashboard = () => {
-  return <DashboardClient />;
+const Dashboard = async () => {
+  const store = await getStoreByUserId();
+
+  return <DashboardClient store={store} />;
 };
 export default Dashboard;
