9a8cfea0281b62e174dcae14173c8ae7780b6081
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-08-24 23:49:52 -0400

    create route.ts for deleting a product, used getProductsByUserId for dashboard page

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -7,15 +7,23 @@ import { toast } from "react-hot-toast";
 import { useState } from "react";
 import Input from "../components/inputs/Input";
 import { useRouter } from "next/navigation";
-import { Store } from "@prisma/client";
+import { Product, Store } from "@prisma/client";
 import classes from "./DashboardClient.module.css";
+import ProductsList from "../components/dashboard/ProductsList";
+import AddProductClient from "../components/dashboard/AddProductClient";
 
 interface DashboardClientProps {
   store: Store | null;
+  products: Product[] | null;
 }
 
-const DashboardClient: React.FC<DashboardClientProps> = ({ store }) => {
+const DashboardClient: React.FC<DashboardClientProps> = ({
+  store,
+  products,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [tab, setTab] = useState("productsTab");
+  const [productFormIsOpen, setProductFormIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
@@ -35,6 +43,7 @@ const DashboardClient: React.FC<DashboardClientProps> = ({ store }) => {
       .then(() => {
         toast.success("Store Created!");
         setIsOpen(false);
+        setTab("productsTab");
         router.refresh();
       })
       .catch((error) => {
@@ -47,71 +56,109 @@ const DashboardClient: React.FC<DashboardClientProps> = ({ store }) => {
 
   return (
     <div>
-      <div className={classes.navbar}>
-        <h2>Dashboard</h2>
-        <div className={classes.navActions}>
-          {store && (
-            <button
-              onClick={() => router.push(`/store/${store?.subdomain}`)}
-              className={classes.visitStoreButton}
-            >
-              Visit your Store
-            </button>
-          )}
-          {!store && (
-            <button
-              onClick={() => setIsOpen(true)}
-              className={classes.createStoreFormButton}
-            >
-              Create a Store
-            </button>
-          )}
-          <button onClick={() => signOut()} className={classes.logoutButton}>
-            Sign Out
-          </button>
-        </div>
-      </div>
+      <nav className={classes.navbar}>
+        <h2>FashionVista</h2>
+        <button onClick={() => signOut()} className={classes.logoutButton}>
+          Sign Out
+        </button>
+      </nav>
 
-      {isOpen && (
-        <div>
-          <form onSubmit={handleSubmit(createStore)} className={classes.form}>
-            <h3>Create your store!</h3>
-            <Input
-              id="name"
-              label="Name"
-              disabled={isLoading}
-              register={register}
-              errors={errors}
-              required
-            />
-            <Input
-              id="description"
-              label="Description"
-              disabled={isLoading}
-              register={register}
-              errors={errors}
-              required
-            />
-            <Input
-              id="subdomain"
-              label="Subdomain (letters and numbers only)"
-              disabled={isLoading}
-              register={register}
-              errors={errors}
-              required
-            />
-            <div className={classes.formActions}>
+      {isOpen && tab === "createStore" && (
+        <form onSubmit={handleSubmit(createStore)} className={classes.form}>
+          <h3>Create your store!</h3>
+          <Input
+            id="name"
+            label="Name"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="description"
+            label="Description"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="subdomain"
+            label="Subdomain"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <div className={classes.formActions}>
+            <button
+              onClick={() => {
+                setIsOpen(false);
+                setTab("productsTab");
+              }}
+              className={classes.cancelFormButton}
+            >
+              Cancel
+            </button>
+            <button type="submit" className={classes.createStoreFormButton}>
+              Create
+            </button>
+          </div>
+        </form>
+      )}
+
+      {tab !== "createStore" && (
+        <div className={classes.container}>
+          <div className={classes.lowerNav}>
+            {store && (
               <button
-                onClick={() => setIsOpen(false)}
-                className={classes.cancelFormButton}
+                onClick={() => router.push(`/store/${store?.subdomain}`)}
+                className={classes.visitStoreButton}
               >
-                Cancel
+                Visit your Store
               </button>
-              <button type="submit" className={classes.createStoreButton}>
-                Create
+            )}
+            {!store && (
+              <button
+                onClick={() => {
+                  setIsOpen(true);
+                  setTab("createStore");
+                }}
+                className={classes.createStoreFormButton}
+              >
+                Create a Store
               </button>
+            )}
+            <div className={classes.tabButtons}>
+              <button onClick={() => setTab("productsTab")}>Products</button>
+              <button onClick={() => setTab("ordersTab")}>Orders</button>
             </div>
-          </form>
+          </div>
+          {tab === "productsTab" && (
+            <div className={classes.screen}>
+              <div className={classes.screenHeading}>
+                <h3>Dashboard</h3>
+                {store && (
+                  <button
+                    onClick={() =>
+                      setProductFormIsOpen((prevState) => !prevState)
+                    }
+                    className={classes.addProductButton}
+                  >
+                    {productFormIsOpen ? "Close Form" : "Add Product"}
+                  </button>
+                )}
+              </div>
+
+              {productFormIsOpen && <AddProductClient />}
+
+              {store && (
+                <div className={classes.products}>
+                  <ProductsList products={products} />
+                </div>
+              )}
+            </div>
+          )}
         </div>
       )}
     </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import getProductsByUserId from "../actions/getProductsByUserId";
 import getStoreByUserId from "../actions/getStoreByUserId";
 import DashboardClient from "./DashboardClient";
 
 const Dashboard = async () => {
   const store = await getStoreByUserId();
+  const products = await getProductsByUserId();
 
-  return <DashboardClient store={store} />;
+  return <DashboardClient store={store} products={products} />;
 };
 export default Dashboard;
