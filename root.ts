561743e283c079617f036822ae06f8feb69fe8c4
R. LaRoi <are@raewhoo.com>
2023-08-27 21:41:59 -0400

    Delete root.ts

--- root.ts
+++
@@ -1,5 +0,0 @@
-DATABASE_URL="mongodb+srv://chinguteam43:iixf2TJ5bcV8KUly@cluster0.pqwpj48.mongodb.net/ecommerce"
-
-NEXTAUTH_SECRET='NEXTAUTH_SECRET'
-
-NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME="dhvtk5pnw"
