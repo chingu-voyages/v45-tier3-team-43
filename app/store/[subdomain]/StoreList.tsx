a3fa6bf9cf523b47830793785c0f945a49c1edd8
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-08-31 23:13:41 -0400

    moved search input into StoreList.tsx

--- app/store/[subdomain]/StoreList.tsx
+++ app/store/[subdomain]/StoreList.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@prisma/client";
 import classes from "./StoreList.module.css";
 import StoreItem from "./StoreItem";
+import { LiaSearchSolid } from "react-icons/lia";
 
 interface StoreListProps {
   products: Product[] | null;
@@ -9,7 +10,19 @@ interface StoreListProps {
 const StoreList: React.FC<StoreListProps> = ({ products }) => {
   return (
     <div className={classes.storeList}>
-      <div className={classes.heading}>Products</div>
+      <div className={classes.heading}>
+        <div className={classes.headingTitle}>Products</div>
+        <div className={classes.inputContainer}>
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="Search products..."
+            id="search"
+          />
+          <LiaSearchSolid size={20} />
+        </div>
+      </div>
+
       {(!products || products.length === 0) && (
         <p className={classes.noProducts}>This store has no products yet!</p>
       )}
