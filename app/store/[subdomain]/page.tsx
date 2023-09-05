87654cfd19335daaf5fabebf51be2344d20e4730
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-09-04 21:47:46 -0400

    added dashboard button on store page if current user owns that store

--- app/store/[subdomain]/page.tsx
+++ app/store/[subdomain]/page.tsx
@@ -2,6 +2,7 @@ import getStoreById from "@/app/actions/getStoreById";
 import StorePageClient from "./StorePageClient";
 import getProductsByStoreId from "@/app/actions/getProductsByStoreId";
 import getBannerByStoreId from "@/app/actions/getBannerByStoreId";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 interface IParams {
   subdomain: string;
@@ -11,7 +12,15 @@ const StorePage = async ({ params }: { params: IParams }) => {
   const store = await getStoreById(params);
   const products = await getProductsByStoreId(params);
   const banner = await getBannerByStoreId(params);
+  const currentUser = await getCurrentUser();
 
-  return <StorePageClient store={store} products={products} banner={banner} />;
+  return (
+    <StorePageClient
+      store={store}
+      products={products}
+      banner={banner}
+      currentUser={currentUser}
+    />
+  );
 };
 export default StorePage;
